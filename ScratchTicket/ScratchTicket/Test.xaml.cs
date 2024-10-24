8f3e376f230adcbc2ec29d483fbf3d8fecd6ce56
hlz2516 <714251494@qq.com>
2024-10-24 22:34:58 +0800

    开始设计刮刮功能原型

--- ScratchTicket/ScratchTicket/Test.xaml.cs
+++ ScratchTicket/ScratchTicket/Test.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
 
 namespace ScratchTicket
@@ -23,10 +26,24 @@ namespace ScratchTicket
         {
             InitializeComponent();
         }
-
-        private void CardHolder_CardClick(object sender, RoutedEventArgs e)
+        private void RenderButton_Click(object sender, RoutedEventArgs e)
         {
+            Image myImage = new Image();
+            FormattedText text = new FormattedText("ABC",
+                    new CultureInfo("en-us"),
+                    FlowDirection.LeftToRight,
+                    new Typeface(this.FontFamily, FontStyles.Normal, FontWeights.Normal, new FontStretch()),
+                    36,
+                    this.Foreground);
 
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+            drawingContext.DrawText(text, new Point(2, 2));
+            drawingContext.Close();
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap(180, 180, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+            RenderedImage.Source = bmp;
         }
     }
 }
